9612d8ed351dd0c86d73664261663e32d44ed0c8
adrickson <144737887+adrickson@users.noreply.github.com>
2024-03-16 22:10:41 -0300

    animação JS feita para versão desktop

--- SITE VENDAS/js/animacoes.js
+++ SITE VENDAS/js/animacoes.js
@@ -1,32 +1,41 @@
 
-// ANIMAÇÃO SHADOW
-
-
+// ANIMAÇÃO DE ESCALA
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    var caixasSes1 = document.querySelectorAll(".caixasSes1")
+    var caixaSes1 = document.querySelectorAll('.caixasSes12')
 
-    if(caixasSes1.length > 0){
-
-        setTimeout(() =>{
-
-            caixasSes1.forEach(caixa => {
-                caixa.classList.add("shadow")
-
-                console.log(caixa)
-            })
-
-        }, 2000)    
-        
-        setTimeout(() => {
-            caixasSes1.forEach(caixa => {
-                caixa.classList.remove("shadow")
-            })
-        }, 3000)
-
+    if(caixaSes1.length > 0){
+        removeAdicionaClass(caixaSes1)
+        console.log(caixaSes1)
     } else {
-        console.log('nenhum elemento encontrado')
+        console.log('Nenhum elemento encontrado!')
     }
 
 })
+
+function removeAdicionaClass(caixas){
+
+    caixas.forEach(caixa => {
+
+        caixa.classList.add('animScale')
+
+    })
+
+    setTimeout(() => {
+        
+        caixas.forEach(caixa => {
+
+            caixa.classList.remove('animScale')
+
+        })
+
+        setTimeout(() => {
+            removeAdicionaClass(caixas) // Chamada recursiva para ser repetida a função 
+        }, 7000) // aguarda 7s  para começar novamente
+
+    }, 3500) //Remove a classe após 3 segundos
+
+}
+
+// FIM DA ANIMAÇÃO DE ESCALA
